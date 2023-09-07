4c5c205637948bedbc990cb1f5e43031dc366623
LudovicoPellecchia <ludovico.pellecchia@gmail.com>
2023-09-07 17:28:38 +0200

    aggiunta tabella

---
+++ index.php
@@ -0,0 +1,97 @@
+<?php
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1 class="text-center">PHP - HOTEL</h1>
+
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance To Center</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($hotels as $hotel) {
+
+                ?>
+                    <tr>
+                        <td><?php echo $hotel["name"] ?></td>
+                        <td><?php echo $hotel["description"] ?></td>
+                        <td><?php echo $hotel["parking"] ? "Yes" : "No"; ?></td>
+                        <td><?php echo $hotel["vote"] ?></td>
+                        <td><?php echo $hotel["distance_to_center"] ?></td>                    
+                    </tr>
+
+                <?php
+                }
+                ?>
+
+            </tbody>
+        </table>
+
+    </div>
+
+
+</body>
+
+</html>
